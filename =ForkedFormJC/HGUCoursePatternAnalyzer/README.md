be1a98e8187e156596991805a64b0dad45ab10f7
Jinwoo Nam <21600212@handong.edu>
2021-04-21 13:30:23 +0900

    Delete =ForkedFormJC/HGUCoursePatternAnalyzer directory

--- =ForkedFormJC/HGUCoursePatternAnalyzer/README.md
+++
@@ -1,2 +0,0 @@
-# HGUCoursePatternAnalyzer
-HGU Course Pattern Analyzer
--- =ForkedFormJC/HGUCoursePatternAnalyzer/src/main/java/edu/handong/analysis/Main.java
+++
@@ -1,9 +0,0 @@
-package edu.handong.analysis;
-
-public class Main {
-
-	public static void main(String[] args) {
-		HGUCoursePatternAnalyzer analyzer = new HGUCoursePatternAnalyzer();
-		analyzer.run(args);
-	}
-}
